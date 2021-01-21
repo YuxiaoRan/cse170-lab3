b4c14e2dc1b302b96cf2a7fda9f8f0679e7e8170
ranyuxiao <ranyuxiao@bytedance.com>
2021-01-22 03:01:40 +0800

    finish lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,32 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron h1").text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$(".project").click(projectClick);
+}
+
+function projectClick(e) {
+	console.log("Project clicked");
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+  // containingProject.append(
+	// 	"<div class='project-description'><p>Description of the project.</p></div>"
+	// );
+	var description = $(containingProject).find(".project-description");
+  if (description.length == 0) {
+     $(containingProject).append(
+			 "<div class='project-description'><p>Description of the project.</p></div>"
+		 );
+  } else {
+     // description.html(
+			//  "<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>"
+		 // );
+		 description.fadeToggle();
+  }
 }
